15aaa649071e7370dfeba4cd39314da3835cbf07
Morten Svendgård <mortensvendgaard@gmail.com>
2024-08-08 10:47:27 +0200

    Script updates

--- Conversion Script/functions/output_parameters.py
+++ Conversion Script/functions/output_parameters.py
@@ -1,22 +1,15 @@
 import os
 import pandas as pd
 
-def output_regular_parameters(dataframes_dict, output_directory, output_excel_file_path, output_csv_directory, output_file_format, scenario_option):
+def output_regular_parameters(dataframes_dict, output_directory, output_excel_file_path):
     # Ensure the output directory exists
     os.makedirs(output_directory, exist_ok=True)
-
-    if output_file_format == 'excel':
         
-        # Sort worksheet names alphabetically, but keep 'Sets' at the beginning
-        sorted_worksheet_names = ['Sets'] + sorted([name for name in dataframes_dict if name != 'Sets'])
+    # Sort worksheet names alphabetically, but keep 'Sets' at the beginning
+    sorted_worksheet_names = ['Sets'] + sorted([name for name in dataframes_dict if name != 'Sets'])
 
-        # Write to Excel file
-        with pd.ExcelWriter(output_excel_file_path, engine='xlsxwriter', mode='w') as excel_writer:
-            for worksheet_name in sorted_worksheet_names:
-                df_to_output = dataframes_dict[worksheet_name]
-                df_to_output.to_excel(excel_writer, sheet_name=worksheet_name, index=False)
-    else:
-        # Handle CSV output if required (same as before)
-        for worksheet_name, df in dataframes_dict.items():
-            output_file_path = os.path.join(output_csv_directory, f"{worksheet_name}_{scenario_option}.csv")
-            df.to_csv(output_file_path, index=False)
+    # Write to Excel file
+    with pd.ExcelWriter(output_excel_file_path, engine='xlsxwriter', mode='w') as excel_writer:
+        for worksheet_name in sorted_worksheet_names:
+            df_to_output = dataframes_dict[worksheet_name]
+            df_to_output.to_excel(excel_writer, sheet_name=worksheet_name, index=False)
--- Conversion Script/functions/output_timeseries.py
+++ Conversion Script/functions/output_timeseries.py
@@ -1,25 +1,14 @@
 import os
 import pandas as pd
 
-def output_timeseries(filtered_data, output_excel_directory, output_excel_file_path_timeseries, output_csv_directory, output_file_format, scenario_option):
+def output_timeseries(filtered_data, output_excel_directory, output_excel_file_path_timeseries):
     # Determine the output directory based on the file format
-    if output_file_format == 'excel':
-        output_directory = output_excel_directory
-        output_file_path = output_excel_file_path_timeseries
-    else:
-        output_directory = output_csv_directory
+    output_directory = output_excel_directory
+    output_file_path = output_excel_file_path_timeseries
 
     # Ensure the output directory exists
     os.makedirs(output_directory, exist_ok=True)
-
-    if output_file_format == 'excel':
-        # Open the ExcelWriter
-        with pd.ExcelWriter(output_file_path, engine='xlsxwriter', mode='w') as writer:
-            for sheet_name, df in filtered_data.items():
-                df.to_excel(writer, sheet_name=sheet_name, index=False)
-
-
-    else:
-        for file_name, df in filtered_data.items():
-            output_file_path = os.path.join(output_directory, f"{file_name}_{scenario_option}.csv")
-            df.to_csv(output_file_path, index=False)
+    # Open the ExcelWriter
+    with pd.ExcelWriter(output_file_path, engine='xlsxwriter', mode='w') as writer:
+        for sheet_name, df in filtered_data.items():
+            df.to_excel(writer, sheet_name=sheet_name, index=False)
