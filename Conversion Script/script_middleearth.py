38320d4f2a47ec5b81e204ab42dbd4b1d63253ba
Konstantin Löffler <53045143+loeffko@users.noreply.github.com>
2025-08-19 20:25:43 +0200

    Add files via upload

---
+++ Conversion Script/script_middleearth.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+settings_file = 'Set_filter_file_MiddleEarth.xlsx'
+output_file_format = 'excel' 
+output_format = 'long' 
+processing_option = 'both' 
+scenario_option = 'MiddleEarth'
+debugging_output = False 
+data_base_region = 'Gondor'  
+
+from functions.function_import import master_function
+
+master_function(settings_file,output_file_format, output_format, processing_option, scenario_option, debugging_output, data_base_region)
